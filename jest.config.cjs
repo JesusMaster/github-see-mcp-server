13ba1af5e8f8a9dc7583e2143ac2b4217bf0b241
Emanuel Jesus Leiva Navarro <saxofonista7@gmail.com>
2025-08-26 16:43:14 -0400

    fix: sonarqube issues

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,20 +1,27 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  extensionsToTreatAsEsm: ['.ts'],
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
-  collectCoverage: true,
-  collectCoverageFrom: ['**/*.{js,ts}', '!**/node_modules/**'],
-  coverageDirectory: 'coverage',
-  // SOLO ts, nunca js
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
+  roots: ['<rootDir>'],
+  testMatch: ['<rootDir>/test/**/*.test.[jt]s?(x)'],
   moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
     '^#controllers/(.*)$': '<rootDir>/controllers/$1',
-    '^#tools/(.*)$': '<rootDir>/tools/$1'
+    '^#tools/(.*)$': '<rootDir>/tools/$1',
   },
-  // quita esto si no usas ESM en tests
-  // extensionsToTreatAsEsm: ['.ts'],
-  moduleFileExtensions: ['js', 'ts'],
-  testEnvironmentOptions: { url: 'http://localhost/' }
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'controllers/**/*.ts',
+    'tools/**/*.ts',
+    '!**/node_modules/**',
+    '!**/test/**'
+  ],
 };
